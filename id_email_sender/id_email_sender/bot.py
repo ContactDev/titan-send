b90fcc3e2a9880e24be119bf58b1c0d8d45f6442
Eliakim Paulino França | T.I <efranca@CONTACTMAIS.INTRA>
2022-10-17 11:28:16 -0300

    atualização de parâmetros

--- id_email_sender/id_email_sender/bot.py
+++ id_email_sender/id_email_sender/bot.py
@@ -11,7 +11,7 @@ class Bot(DesktopBot):
         for i in tabela.index:
 
             email = str(tabela.loc[i, 'email'])
-            subject = str(tabela.loc[i, 'subject'])
+            subject = 'Novidades - ALELO Natal'
             message = """Olá somos a ALELO e temos Novidades!!!
 
 Estamos próximos do Natal.Você já pensou em transformar a expectativa do seu colaborador em realidade?
@@ -31,23 +31,24 @@ Faça uma Consulta e entre em Contato Conosco."""
             if self.find( "writtingFind", matching=0.97, waiting_time=30000):
                 self.not_found("writtingFind")
                 self.click()
-                self.wait(1000)
+            self.sleep(1000)
 
             if self.find( "newEmail", matching=0.97, waiting_time=10000):
                 self.not_found("newEmail")
 
             self.paste(email)
-            self.wait(500)
+            self.sleep(500)
             self.tab()
             self.paste(subject)
-            self.wait(500)
+            self.sleep(500)
             self.tab()
             self.paste(message)
-            self.wait(500)
+            self.sleep(500)
                        
             if self.find( "sendFind", matching=0.97, waiting_time=10000):
                 self.not_found("sendFind")
             self.click()
+            self.sleep(1440)
 
         print('====Fim====')
             
@@ -56,7 +57,3 @@ Faça uma Consulta e entre em Contato Conosco."""
 
 if __name__ == '__main__':
     Bot.main()
-
-
-
-
